1c7a70c35cdf902a5d03f393733c949d15fea9d8
AlexanderFriedrich <friedrich.alexander@gmx.at>
2024-07-26 09:35:41 +0200

    finanzrechner fertig. TODO: Documentation

--- Finanzrechner/Source/Controllers/StatisticController.cs
+++ Finanzrechner/Source/Controllers/StatisticController.cs
@@ -15,27 +15,68 @@ namespace Finanzrechner.Controllers
             _context = context;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(DateTime? dateFrom, DateTime? dateTo)
         {
-            StatisticData statisticData = new();
+            List<Transaction> transactions = _context.Transactions.ToList();
 
-            await _context.Transactions.Where(x => x.IsIntake == true).OrderByDescending(x => x.Amount).Take(5).ForEachAsync(x =>
+            if (dateFrom is not null)
             {
-                if (x.Description is null)
-                    x.Description = "KEINE BESCHREIBUNG";
+                ViewBag.DateFromFilter = dateFrom.Value.ToString("yyyy-MM-dd");
+                ViewBag.ShowDeleteFilterButton = true;
+                transactions = transactions.Where(x => x.TimeStamp >= dateFrom).ToList();
+            }
 
-                statisticData.Top5Instakes.Add(new ColumnChartData
-                {
-                    name = x.Description,
-                    y = (double)x.Amount
-                });
+            if (dateTo is not null)
+            {
+                ViewBag.DateToFilter = dateTo.Value.ToString("yyyy-MM-dd");
+                ViewBag.ShowDeleteFilterButton = true;
+                transactions = transactions.Where(x => x.TimeStamp <= dateTo).ToList();
+            }
+
+
+
+            StatisticData statisticData = new();
+            statisticData.CountOfIntakes = transactions.Where(x => x.IsIntake == true).Count();
+            statisticData.SumOfIntakes = transactions.Where(x => x.IsIntake == true).Sum(x => x.Amount);
+            statisticData.CountOfOuttakes = transactions.Where(x => x.IsIntake == false).Count();
+            statisticData.SumOfOuttakes = transactions.Where(x => x.IsIntake == false).Sum(x => x.Amount);
+
+
+
+            List<Transaction> top5intakeTransactions = transactions.Where(x => x.IsIntake == true).OrderByDescending(x => x.Amount).Take(5).ToList();
+            List<Transaction> top5outtakeTransactions = transactions.Where(x => x.IsIntake == false).OrderByDescending(x => x.Amount).Take(5).ToList();
+
+            statisticData.Top5Outtakes.Series.Add(new ColumnChartSeries
+            {
+                name = "Ausgaben"
             });
 
+            foreach (Transaction transaction in top5outtakeTransactions)
+            {
+                if (transaction.Description is null)
+                    transaction.Description = "KEINE BESCHREIBUNG";
 
-            statisticData.CountOfIntakes = _context.Transactions.Where(x => x.IsIntake == true).Count();
-            statisticData.SumOfIntakes = _context.Transactions.Where(x => x.IsIntake == true).Sum(x => x.Amount);
-            statisticData.CountOfOuttakes = _context.Transactions.Where(x => x.IsIntake == false).Count();
-            statisticData.SumOfOuttakes = _context.Transactions.Where(x => x.IsIntake == false).Sum(x => x.Amount);
+                statisticData.Top5Outtakes.Categories.Add(transaction.Description);
+
+                ColumnChartSeries series = statisticData.Top5Outtakes.Series.Where(x => x.name == "Ausgaben").First();
+                series.data.Add((double)transaction.Amount);
+            }
+
+            statisticData.Top5Intakes.Series.Add(new ColumnChartSeries
+            {
+                name = "Einnahmen"
+            });
+
+            foreach (Transaction transaction in top5intakeTransactions)
+            {
+                if (transaction.Description is null)
+                    transaction.Description = "KEINE BESCHREIBUNG";
+
+                statisticData.Top5Intakes.Categories.Add(transaction.Description);
+
+                ColumnChartSeries series = statisticData.Top5Intakes.Series.Where(x => x.name == "Einnahmen").First();
+                series.data.Add((double)transaction.Amount);
+            }
 
             return View(statisticData);
         }
--- Finanzrechner/Source/Models/StatisticData.cs
+++ Finanzrechner/Source/Models/StatisticData.cs
@@ -4,15 +4,22 @@
     {
         public int CountOfIntakes { get; set; }
         public decimal SumOfIntakes { get; set; }
+        public ColumnChartData Top5Intakes { get; set; } = new ColumnChartData();
         public int CountOfOuttakes { get; set; }
         public decimal SumOfOuttakes { get; set; }
-
-        public List<ColumnChartData> Top5Instakes { get; set; } = new List<ColumnChartData>();
+        public ColumnChartData Top5Outtakes { get; set; } = new ColumnChartData();
     }
 
     public class ColumnChartData 
     {
+
+        public List<string> Categories { get; set; } = new List<string>();
+        public List<ColumnChartSeries> Series { get; set; } = new List<ColumnChartSeries>();
+    }
+
+    public class ColumnChartSeries
+    {
         public string name { get; set; }
-        public double y { get; set; }
+        public List<double> data { get; set; } = new List<double>();
     }
 }
